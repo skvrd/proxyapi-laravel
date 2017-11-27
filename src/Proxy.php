ab8f643e86c339d9730fafd04c51d4ac53b82294
Grigoriy Romanyuk <gr@Grigoriys-MacBook-Pro.local>
2017-11-26 20:36:27 -0800

    version 0.1.0 release

--- src/Proxy.php
+++ src/Proxy.php
@@ -1,14 +1,24 @@
 <?php
 
 namespace Skvrd\ProxyAPI;
+use GuzzleHttp\Client;
 
 class Proxy {
     
-    public function __construct() {
+    private $proxy;
 
+    public function __construct() {
+        $client = new Client([
+            // This have to move to config i guess
+            'timeout'  => 5.0,
+        ]);
+        $response = $client->get('http://proxyapi.me/api/proxy');
+        $body = (string) $response->getBody();
+        $proxy_object = json_decode($body);
+        $this->proxy = $proxy_object->data;
     }
     
     public function get() {
-        return "Test";
+        return $this->proxy;
     }
 }
